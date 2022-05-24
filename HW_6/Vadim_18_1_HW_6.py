2dc8f039c9fc776c873b406a7efd7e750ecbee7a
artoshka <105043407+artoshka@users.noreply.github.com>
2022-05-24 20:04:31 +0300

    Update Vadim_18_1_HW_6.py

--- HW_6/Vadim_18_1_HW_6.py
+++ HW_6/Vadim_18_1_HW_6.py
@@ -1,5 +1,5 @@
 def get_list() -> list:
-    return list(range(0, 1_000_000_000, 2))
+    return list(range(0, 1_000_000, 2))
 
 
 """
@@ -13,17 +13,26 @@ class Solution:
         find_target -> YOUR_CODE
     """
 
-    def find_target(self, list, target):
-        for num in range(len(list)):
-            if list[num] == target:
-                print(f'objact {target} on {num}')
+    def find_target(self, target: int, mass: list) -> int:
+        low = 0
+        high = len(mass) - 1
+
+        while low <= high:
+            midle = (low + high) // 2
+            if mass[midle] < target:
+                low = midle - 1
+            elif mass[midle] > target:
+                high = midle + 1
+            else:
+                return midle
+        return -1
 
 
-l = get_list()
 
 
+value = Solution().find_target(400000, get_list())
 
+print(value)
 
-Solution.find_target("", l, 50_000_000)
 
 
